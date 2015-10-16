b8a253bb479a6bdaccab899d9f314eec369df85b
Tan Xiang <tanxiang>
2015-10-16 11:43:49 +0800

    PreferenceActivity

--- src/org/ttand/nhudp/bhconfig.java
+++ src/org/ttand/nhudp/bhconfig.java
@@ -1,15 +1,17 @@
 package org.ttand.nhudp;
 
 import android.app.Activity;
+import android.preference.PreferenceActivity;
 import android.os.Bundle;
 
-public class bhconfig extends Activity
+public class bhconfig extends PreferenceActivity
 {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        //setContentView(R.layout.main);
+        addPreferencesFromResource(R.xml.pref);  
     }
 }
---
+++ src/org/ttand/nhudp/preferences.java
@@ -0,0 +1,10 @@
+package org.ttand.nhudp;
+import android.preference.ListPreference;
+import android.content.Context;
+import android.util.AttributeSet;
+
+public class preferences extends ListPreference{
+    public preferences(Context context,AttributeSet attr){
+    	super(context,attr);
+    }
+}
