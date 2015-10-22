fe073304c725db0f4dce78b8e99a40f41a184c8b
Tan Xiang <tanxiang>
2015-10-22 20:45:37 +0800

    style HW

--- src/org/ttand/nhudp/bhconfig.java
+++ src/org/ttand/nhudp/bhconfig.java
@@ -2,8 +2,11 @@ package org.ttand.nhudp;
 
 import android.app.Activity;
 import android.preference.PreferenceActivity;
+import android.preference.PreferenceFragment;
+import android.preference.PreferenceManager;
 import android.os.Bundle;
 import android.view.Menu;
+import java.util.List;
 
 public class bhconfig extends PreferenceActivity
 {
@@ -12,15 +15,27 @@ public class bhconfig extends PreferenceActivity
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        //setContentView(R.layout.main);
-        addPreferencesFromResource(R.xml.pref);
-        getPreferenceScreen().setOrderingAsAdded(true);
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
+    public void onBuildHeaders(List<Header> target) {
+        loadHeadersFromResource(R.xml.pref_head, target);
+    }
+
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.main_menu, menu);
         return true;
+    }
+    public class Prefs1Fragment extends PreferenceFragment {
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            PreferenceManager.setDefaultValues(getActivity(),
+                    R.xml.pref, false);
+            // Load the preferences from an XML resource
+            addPreferencesFromResource(R.xml.pref);
+        }
     }
 
 }
